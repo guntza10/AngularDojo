e6c8cd289339597f242a6e8e8f3a6e9e31374147
jomphop <jomphop@perfenterprise.com>
2020-04-23 17:44:29 +0700

    update

--- angularForm/src/app/app.component.ts
+++ angularForm/src/app/app.component.ts
@@ -10,5 +10,17 @@ export class AppComponent {
   title = 'Angular Form';
   topics = ['Angular', 'React', 'VueJs'];
 
-  userModel = new User('Gun', 'guntza22@hotmail.com', '08451675125', 'angular', 'morning', true);
+  userModel = new User('Gun', 'guntza22@hotmail.com', '0845167512', 'default', 'morning', true);
+
+  // select validate
+  topicError: boolean;
+
+  validateSelect(value) {
+    if (value === 'default') {
+      this.topicError = true;
+    }
+    else {
+      this.topicError = false;
+    }
+  }
 }
