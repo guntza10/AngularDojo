8b8b2ba60b9ab8f77195902635a7d066dd16e03b
jomphop <jomphop@perfenterprise.com>
2020-04-24 17:34:39 +0700

    learn to reacive form with create form model

--- reactiveForm/src/app/app.component.ts
+++ reactiveForm/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactiveForm';
+
+  title = 'Reactive Form';
+  // สร้าง Form model
+  registrationForm = new FormGroup({
+    userName: new FormControl('Akuma'),
+    passWord: new FormControl(''),
+    confirmPassword: new FormControl('')
+  });
+
 }
