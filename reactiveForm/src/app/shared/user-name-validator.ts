5f5684bfdbe4fda238aeaeaa9359dfcfc53daa2d
guntza10 <guntza22@hotmail.com>
2020-04-27 21:27:00 +0700

    update

--- reactiveForm/src/app/shared/user-name-validator.ts
+++ reactiveForm/src/app/shared/user-name-validator.ts
@@ -1,8 +1,10 @@
 import { AbstractControl } from "@angular/forms";
 
 // ตัวจัดการ custom validation
-// function forbiddenNameValidator(control: AbstractControl): { [key: string]: any } | null {
-//     const forbidden = /admin/.test(control.value); // เป็นการ check ว่า value containe string admin มั้ย (เป็นการใช้ regx check)
-//     const forbidde = control.value.toString().include('admin');
-//     return forbidden ? {'forbidden'}
-// }
+export function forbiddenNameValidator(control: AbstractControl): { [key: string]: any } | null {
+    const forbidden = /admin/.test(control.value); // เป็นการ check ว่า value containe string admin มั้ย (เป็นการใช้ regx check)
+    const forbidde = control.value.toString().include('admin');
+    // flag เป็น true (containe admin เป็น true) -> จะ return object value -> inValid
+    // flag เป็น false (ไม่ได้ containe admin เป็น false) -> จะ return null -> valid
+    return forbidden ? { 'forbiddenName': { value: control.value } } : null;
+}
